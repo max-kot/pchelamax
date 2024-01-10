598b4309cf78da4750e8a91c44e5571a1d1e878f
max-kot <max.kotlarenko@gmail.com>
2024-01-10 15:22:43 +0300

    added adaptive to header and first section

--- app/js/modules/menu.js
+++ app/js/modules/menu.js
@@ -1,7 +1,9 @@
+const header = document.querySelector('.header');
 const headerMenuBtn = document.querySelector('.header__menu .menu__btn');
 const headerMenuBox = document.querySelector('.header__menu .menu__box');
 
 headerMenuBtn.addEventListener('click', () => {
+	header.classList.toggle('active');
 	headerMenuBtn.classList.toggle('active');
 	headerMenuBox.classList.toggle('active');
 	document.body.classList.toggle('no-scroll');
@@ -11,6 +13,7 @@ headerMenuBtn.addEventListener('click', () => {
 	
 		allLinks.forEach((link) => {
 			link.addEventListener('click', () => {
+				header.classList.remove('active');
 				headerMenuBtn.classList.remove('active');
 				headerMenuBox.classList.remove('active');
 				document.body.classList.remove('no-scroll');
